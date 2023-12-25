304aa4ceab876553d22524abf1c0312a473e6b65
TANISHA <115376761+tanisha10101@users.noreply.github.com>
2023-12-25 21:11:47 +0530

    try

---
+++ script.js
@@ -0,0 +1,12 @@
+var Typed = new Typed(".text", {
+    strings:["Web Developer", "UI/UX Designer"],
+    typeSpeed: 100,
+    backSpeed: 100, 
+    backDelay: 1000, 
+    loop: true
+    
+
+
+
+
+})
